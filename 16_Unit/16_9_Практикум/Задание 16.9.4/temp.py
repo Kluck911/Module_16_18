954c963f877b4c3d91b95c5a9de1ac42520683af
Kluck911 <kluck1812@gmail.com>
2021-08-13 01:04:31 +0300

    Задание 16.9.4 done + комменты

--- 16_Unit/16_9_Практикум/Задание 16.9.4/temp.py
+++ 16_Unit/16_9_Практикум/Задание 16.9.4/temp.py
@@ -1,23 +1,31 @@
-import datetime
+class Event:
+    def __init__(self, timestamp=0, event_type="", session_id=""):
+        self.timestamp = timestamp
+        self.type = event_type
+        self.session_id = session_id
+
+    def init_from_dict(self, event_dict):
+        self.timestamp = event_dict.get("timestamp")
+        self.type = event_dict.get("type")
+        self.session_id = event_dict.get("session_id")
+
+    def show_description(self):
+        print("This is generic event.")
 
 
-class Product:
-    max_quantity = 100000
+class ItemViewEvent(Event):
+    type = "itemViewEvent"
 
-    def __init__(self, name, category, quantity_in_stock):
-        self.name = name
-        self.category = category
-        self.quantity_in_stock = quantity_in_stock
+    def __init__(self, timestamp=0, session_id="", number_of_views=0):
+        self.timestamp = timestamp
+        self.session_id = session_id
+        self.number_of_views = number_of_views
 
-    def is_available(self):
-        return True if self.quantity_in_stock > 0 else False
-
-
-class Food(Product):
-    is_critical = True
-    needs_to_be_refreshed = True
-    refresh_frequency = datetime.timedelta(days=1)
+    def show_description(self):
+        print("This event means someone has browsed an item.")
 
 
-eggs = Food(name="eggs", category="food", quantity_in_stock=5)
-print(eggs.max_quantity)
+if __name__ == "__main__":
+    test_view_event = ItemViewEvent(timestamp=1549461608000, session_id="0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct", number_of_views=6)
+    test_view_event.show_description()
+    print(test_view_event.type)
