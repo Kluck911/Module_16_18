57126b757b4c70160e5e76583426915c608cf5a6
Kluck911 <kluck1812@gmail.com>
2021-08-12 23:14:49 +0300

    2

--- 16_Unit/16_9_Практикум/Задание 16.9.4/guest_list.py
+++ 16_Unit/16_9_Практикум/Задание 16.9.4/guest_list.py
@@ -1,27 +1,34 @@
-class People:
-    def __init__(self, name, surname, address, status):
+class Volunteer:
+    def __init__(self, name='', surname='', address='', status=''):
         self.name = name
         self.surname = surname
         self.address = address
         self.status = status
 
+    def get_description(self):
+        return '{0} {1}, {2}, статус: {3}'.format(self.name, self.surname, self.address, self.status)
 
-class Guest(People):
+
+class Invited(Volunteer):
     invite_confirmed = True
 
+    def __init__(self, ):
 
-print('Заполните базу сотрудников')
-amount_employees = int(input('Введите количество сотрудников\n'))
 
-name_employee = []
-surname_employee = []
-address_employee = []
-status_employee = []
+def is_guest(volon):
+    print(volon.get_description())
+    verification = input('Данный волонтер приглашен на корпорат? да/нет\n')
+    verification = verification.lower()
+    if verification == 'да':
+        return True
+    else:
+        return False
 
-for i in range(amount_employees):
-    name_employee[i] = input("Введите имя гостя \n")
-    surname_employee[i] = input("Введите фамилию гостя \n")
-    address_employee[i] = input("Введите место проживания гостя  \n")
-    status_employee[i] = input("Введите статус гостя \n")
 
-print(name_employee)
+volunteer_1 = Invited("Антон", "Антонов", "Минск", "укладчик-наладчик")
+volunteer_2 = Volunteer("Степан", "Степанов", "Гродно", "сегодня не пьет")
+volunteer_3 = Volunteer("Кузьма", "Кузьмин", "Брест", "леcник-международник")
+volunteer_4 = Volunteer("Энтони", "Антонов", "Гомель", "слесарь VI- го разряда, в руках ключ на 32")
+volunteer_5 = Volunteer("Святослав", "Аганесян", "Могилев", "прожект менеджер по перетаскиванию кнопки")
+
+volunteer_2 = Invited(volunteer_2)
