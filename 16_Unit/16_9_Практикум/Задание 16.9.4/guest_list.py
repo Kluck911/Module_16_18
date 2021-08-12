380be7329e30c66de7c762abfe43ca759a5def27
Kluck911 <kluck1812@gmail.com>
2021-08-13 00:08:33 +0300

    3

--- 16_Unit/16_9_Практикум/Задание 16.9.4/guest_list.py
+++ 16_Unit/16_9_Практикум/Задание 16.9.4/guest_list.py
@@ -12,8 +12,6 @@ class Volunteer:
 class Invited(Volunteer):
     invite_confirmed = True
 
-    def __init__(self, ):
-
 
 def is_guest(volon):
     print(volon.get_description())
@@ -25,10 +23,29 @@ def is_guest(volon):
         return False
 
 
-volunteer_1 = Invited("Антон", "Антонов", "Минск", "укладчик-наладчик")
+def make_list_guests(volunteer):
+    if is_guest(volunteer):
+        volunteer = Invited(volunteer)
+        list_guests.append(volunteer.get_description())
+    return volunteer
+
+
+# База волонтеров проекта
+
+
+volunteer_1 = Volunteer("Антон", "Антонов", "Минск", "укладчик-наладчик")
 volunteer_2 = Volunteer("Степан", "Степанов", "Гродно", "сегодня не пьет")
 volunteer_3 = Volunteer("Кузьма", "Кузьмин", "Брест", "леcник-международник")
 volunteer_4 = Volunteer("Энтони", "Антонов", "Гомель", "слесарь VI- го разряда, в руках ключ на 32")
 volunteer_5 = Volunteer("Святослав", "Аганесян", "Могилев", "прожект менеджер по перетаскиванию кнопки")
 
-volunteer_2 = Invited(volunteer_2)
+
+list_guests = []
+
+volunteer_1 = make_list_guests(volunteer_1)
+volunteer_2 = make_list_guests(volunteer_2)
+volunteer_3 = make_list_guests(volunteer_3)
+volunteer_4 = make_list_guests(volunteer_4)
+volunteer_5 = make_list_guests(volunteer_5)
+
+print(volunteer_1.get_description())
