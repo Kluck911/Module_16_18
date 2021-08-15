3e64dffa1b147ebc31b28474086e16dab818aa56
Kluck911 <kluck1812@gmail.com>
2021-08-15 12:55:38 +0300

    Задание 16.10

--- 16_Unit/16_10_exeptions/Temp.py
+++ 16_Unit/16_10_exeptions/Temp.py
@@ -1,10 +1,26 @@
-class ParentExeption(Exceptions):
+class ParentException(Exception):
+    def __init__(self, message,
+                 error):  # допишем к нашему пустому классу конструктор, который будет печатать дополнительно в консоль информацию об ошибке.
+        super().__init__(message)  # помним про вызов конструктора родительского класса
+        print(f"Errors: {error}")  # печатаем ошибку
+
+
+class ChildException(ParentException):  # создаём пустой класс – исключение наследника, наследуемся от ParentException
+    def __init__(self, message, error):
+        super().__init__(message, error)
+
+
+class NonPositiveDigitException(ValueError):
     pass
 
-class ChildExeption(ParentExeption):
-    pass
+
+class Square:
+    def __init__(self, a):
+        if a < 0:
+            raise NonPositiveDigitException('Неправильно указанна сторона квадрата')
+
 
 try:
-    raise ChildExeption('message')
-except ParentExeption as e:
-    print(e)
+    raise ChildException("message", "error")  # поднимаем исключение-наследник, передаём дополнительный аргумент
+except ParentException as e:
+    print(e)  # выводим информацию об исключении
